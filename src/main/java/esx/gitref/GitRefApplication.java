290ce17961b987bc8ab93406148ec6756e5ef919
Jurandir C. Nogueira <netocaligrafia@gmail.com>
2020-08-01 18:27:46 -0300

    sync

---
+++ src/main/java/esx/gitref/GitRefApplication.java
@@ -0,0 +1,13 @@
+package esx.gitref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/esx/gitref/GitRefApplicationTests.java
@@ -0,0 +1,13 @@
+package esx.gitref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
